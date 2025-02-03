6cc60b1c1bcf914fb894033940d340a43888ed8a
Aravind6023 <c23aravind@gmail.com>
2025-02-03 08:12:48 +0530

    Updated Formik Components using props

--- src/pages/FormikComponents.jsx
+++ src/pages/FormikComponents.jsx
@@ -5,7 +5,9 @@ const initialValues = {
   //name should match with input name="name"
   name: "",
   email: "",
-  password: "",
+  postName: "",
+  postMessage: "",
+  shareTo: "",
 };
 
 const onSubmit = (values) => {
@@ -15,7 +17,9 @@ const onSubmit = (values) => {
 const validationSchema = Yup.object({
   name: Yup.string().required("Required"),
   email: Yup.string().email("Invalid email format").required("Required"),
-  password: Yup.string().required("Required"),
+  postName: Yup.string().required("Required"),
+  postMessage: Yup.string().required("Required"),
+  shareTo: Yup.string().required("Required"),
 });
 
 //Instead of using the {...formik.getFieldProps("name")} we can use the components from formik
@@ -37,9 +41,27 @@ const FormikComponents = () => {
         </div>
 
         <div className="form-control">
-          <label htmlFor="password">Password</label>
-          <Field type="password" id="password" name="password" />
-          <ErrorMessage name="password" />
+          <label htmlFor="postName">Post Name</label>
+          <Field type="text" id="postName" name="postName" />
+          <ErrorMessage name="postName" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="postMessage">Post Message</label>
+          <Field as="textarea" id="postMessage" name="postMessage" />
+          <ErrorMessage name="postMessage" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="shareTo">Share To</label>
+          <Field name="shareTo">
+            {({ field, meta }) => (
+              <div>
+                <input type="text" id="shareTo" {...field} />
+                {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+              </div>
+            )}
+          </Field>
         </div>
 
         <button type="submit">Submit</button>
